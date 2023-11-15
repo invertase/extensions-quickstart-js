26e2e762b9a50a21b79fee678a4965e4416826ff
Majid <mhadaily@gmail.com>
2023-11-15 14:39:29 +0100

    initial commit with Label video extension example

---
+++ src/helpers/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBltdm_85Dn4MqQtC5nCUK7eV2lkQtmOhM',
+	authDomain: 'devrel-extensions-testing.firebaseapp.com',
+	projectId: 'devrel-extensions-testing',
+	storageBucket: 'devrel-extensions-testing.appspot.com',
+	messagingSenderId: '859145064463',
+	appId: '1:859145064463:web:120f24d3d1ac7e153c937a',
+	measurementId: 'G-95NZVPRQQQ',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storageRef = getStorage(app);
+const analyticsRef = getAnalytics(app);
+
+export { storageRef, analyticsRef };
