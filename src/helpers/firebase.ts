a60af674972fc6c68b418a2144d0b9ae7282dc48
Darren Ackers <ackers86@hotmail.com>
2023-11-28 15:28:58 +0000

    feat: added sematic search with vertex ai example

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -1,16 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-import { getStorage } from 'firebase/storage';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyBltdm_85Dn4MqQtC5nCUK7eV2lkQtmOhM',
-	authDomain: 'devrel-extensions-testing.firebaseapp.com',
-	projectId: 'devrel-extensions-testing',
-	storageBucket: 'devrel-extensions-testing.appspot.com',
-	messagingSenderId: '859145064463',
-	appId: '1:859145064463:web:120f24d3d1ac7e153c937a',
-	measurementId: 'G-95NZVPRQQQ',
+  apiKey: "AIzaSyBltdm_85Dn4MqQtC5nCUK7eV2lkQtmOhM",
+  authDomain: "devrel-extensions-testing.firebaseapp.com",
+  projectId: "devrel-extensions-testing",
+  storageBucket: "devrel-extensions-testing.appspot.com",
+  messagingSenderId: "859145064463",
+  appId: "1:859145064463:web:120f24d3d1ac7e153c937a",
+  measurementId: "G-95NZVPRQQQ",
 };
 
 // Initialize Firebase
@@ -18,5 +20,7 @@ const app = initializeApp(firebaseConfig);
 const storageRef = getStorage(app);
 const firestoreRef = getFirestore(app);
 const analyticsRef = getAnalytics(app);
+const functionsRef = getFunctions(app);
+const authRef = getAuth(app);
 
-export { storageRef, analyticsRef, firestoreRef };
+export { storageRef, analyticsRef, firestoreRef, authRef, functionsRef, app };
